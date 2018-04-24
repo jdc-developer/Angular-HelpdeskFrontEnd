43bcf4db50f0a6c14fd6b1e2c2b4067db5f2aacb
Jorge Do Carmo <tidesconfie@gmail.com>
2018-04-24 13:06:54 -0300

    Implementação do serviço de usuário

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,34 @@
+import { User } from './../models/user';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HELP_DESK_API } from './helpdesk.api';
+
+@Injectable()
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  login(user: User) {
+    return this.http.post(`${HELP_DESK_API}/api/auth`, user);
+  }
+
+  createOrUpdate(user: User) {
+    if (user.id != null && user.id !== 0) {
+      return this.http.put(`${HELP_DESK_API}/api/user`, user);
+    } else {
+      user.id = null;
+      return this.http.post(`${HELP_DESK_API}/api/user`, user);    }
+  }
+
+  findAll(page: number, count: number) {
+    return this.http.get(`${HELP_DESK_API}/api/user/${page}/${count}`);
+  }
+
+  findById(id: number) {
+    return this.http.get(`${HELP_DESK_API}/api/user/${id}`);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${HELP_DESK_API}/api/user/${id}`);
+  }
+}
